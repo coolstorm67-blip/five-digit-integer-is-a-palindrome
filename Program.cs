b8f135467e8b4ee4c020474901a8ee9c40a4b4a2
coolstorm67-blip <coolstorm67@gmail.com>
2026-09-07 10:25:20 +0100

    Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class palindrome
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Enter a five-digit integer:");
+            string number = Console.ReadLine();
+            if (number.Length != 5)
+            {
+                Console.WriteLine("Error: Please enter a five-digit integer.");
+                continue;
+            }
+            if (number[0] == number[4] && number[1] == number[3])
+            {
+                Console.WriteLine("The number is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The number is not a palindrome.");
+            }
+            break;
+        }
+    }
+}
